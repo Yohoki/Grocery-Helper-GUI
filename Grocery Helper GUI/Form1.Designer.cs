ac3017cce6651542ae6f5f200dcd1046029d18cb
Yohoki <Yohoki3@aol.com>
2021-03-10 06:26:40 -0600

    Add project files.

---
+++ Grocery Helper GUI/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace Grocery_Helper_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            this.button1 = new System.Windows.Forms.Button();
+            listView1 = new System.Windows.Forms.ListView();
+            this.ColumnItemName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnItemCat = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnItemSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnItemPrice = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnItemMeals = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnItemPPS = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnItemPPM = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Test Show List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ColumnItemName,
+            this.ColumnItemCat,
+            this.ColumnItemSize,
+            this.ColumnItemPrice,
+            this.ColumnItemMeals,
+            this.ColumnItemPPS,
+            this.ColumnItemPPM});
+            listView1.HideSelection = false;
+            listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            listView1.Location = new System.Drawing.Point(342, 12);
+            listView1.Name = "listView1";
+            listView1.Size = new System.Drawing.Size(657, 426);
+            listView1.TabIndex = 1;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ColumnItemName
+            // 
+            this.ColumnItemName.Text = "ItemName";
+            this.ColumnItemName.Width = 124;
+            // 
+            // ColumnItemCat
+            // 
+            this.ColumnItemCat.Text = "ItemCat";
+            this.ColumnItemCat.Width = 62;
+            // 
+            // ColumnItemSize
+            // 
+            this.ColumnItemSize.Text = "ItemSize";
+            // 
+            // ColumnItemPrice
+            // 
+            this.ColumnItemPrice.Text = "ItemPrice";
+            // 
+            // ColumnItemMeals
+            // 
+            this.ColumnItemMeals.Text = "ItemMeals";
+            // 
+            // ColumnItemPPS
+            // 
+            this.ColumnItemPPS.Text = "ItemPPS";
+            // 
+            // ColumnItemPPM
+            // 
+            this.ColumnItemPPM.Text = "ItemPPM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 450);
+            this.Controls.Add(listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader ColumnItemName;
+        private System.Windows.Forms.ColumnHeader ColumnItemCat;
+        private System.Windows.Forms.ColumnHeader ColumnItemSize;
+        private System.Windows.Forms.ColumnHeader ColumnItemPrice;
+        private System.Windows.Forms.ColumnHeader ColumnItemMeals;
+        private System.Windows.Forms.ColumnHeader ColumnItemPPS;
+        private System.Windows.Forms.ColumnHeader ColumnItemPPM;
+        public static System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Grocery Helper GUI/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Grocery_Helper_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Item> ItemList = Item.Load();
+            //listView1.Items.Add(new ListViewItem(ItemList.ToString()));
+        }
+    }
+
+    public class Item : IEquatable<Item>, IComparable<Item>//, IEnumerable<Item>
+    {
+        //Use **ItemList.Add(new Item() { ItemName = "Test", ItemCat="snackos", ItemSize = 6, ItemPrice = 5.00, ItemMeals = 2 });** to set values
+        //Set ItemName in ItemsList
+        public string ItemName { get; set; }
+
+        //Set ItemCat in ItemsList
+        public string ItemCat { get; set; }
+
+        //Set ItemMeals in ItemsList
+        public double ItemMeals { get; set; }
+
+        //Set ItemSize in ItemsList
+        public double ItemSize { get; set; }
+
+        //Set ItemPrice in ItemsList
+        public double ItemPrice { get; set; }
+
+
+        //Use WriteLine(ItemList) to display this string.
+        public override string ToString()
+        {
+            return (
+                      ItemName.PadRight(21, ' ')
+                    + ItemCat.PadRight(8, ' ')
+                    + ItemSize.ToString().PadRight(8, ' ')
+                    + ItemPrice.ToString("C").PadRight(8, ' ')
+                    + ItemMeals.ToString().PadRight(8, ' ')
+                    + (ItemPrice / ItemSize).ToString("C").PadRight(8, ' ')
+                    + (ItemPrice / ItemMeals).ToString("C").PadRight(8, ' ')
+                    );
+        }
+
+        //Use **ItemList.Remove(new Item() { ItemName = "Chex Mix" });** to check if item can be deleted.
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            Item objAsItem = obj as Item;
+            if (objAsItem == null) return false;
+            else return Equals(objAsItem);
+        }
+
+        // Not sure if needed. Can get hash from ItemNames, but not really useful for this project.
+        // Maybe in other projects, I could use an ID and return that insead of a hash.
+        // Use **ItemList.GetHashCode();** or **ItemList[1].GetHashCode()** to hash entire list or single item.
+        public override int GetHashCode()
+        {
+            return 0;
+            //return HashCode.Combine(ItemName);
+        }
+
+        //Use** ItemList.Remove(new Item() { ItemName = "Chex Mix" });** to delete the whole row.
+        public bool Equals(Item other)
+        {
+            if (other == null) return false;
+            return (this.ItemName.Equals(other.ItemName));
+        }
+
+        //Use **Item.Print(ItemList);** to print entire list.
+        public static List<Item> Print(List<Item> ItemsList)
+        {
+            int ID = 1;
+            String Message = "";
+            Message = ("   ItemName\t\tType\tSize\tPrice\tMeals\t$/Oz\t$/Meal");
+            foreach (Item aItem in ItemsList)
+            {
+                Message = Message + (ID.ToString().PadRight(3, ' ') + aItem + "\n");
+                ID++;
+            }
+            MessageBox.Show(Message);
+            return ItemsList;
+        }
+
+        //Use **Item.Save(ItemList);** to Save ItemsList to a file with '^' delimiters.
+        //CURRENTLY: uses ItemsList[0].ItemCat to determine filename.
+        public static void Save(List<Item> ItemsList)
+        {
+            //string SaveLine ="";
+            //StreamWriter SaveFile = File.AppendText($"./{ItemsList[0].ItemCat}.txt");
+            StreamWriter SaveFile = File.AppendText($"D:/Test/{ItemsList[0].ItemCat}.txt");
+            foreach (Item aItem in ItemsList)
+            {
+                //SaveLine = SaveLine + aItem + "^";
+                SaveFile.WriteLine(
+                      aItem.ItemName + "^"
+                    + aItem.ItemCat + "^"
+                    + aItem.ItemSize + "^"
+                    + aItem.ItemPrice + "^"
+                    + aItem.ItemMeals
+                );
+                SaveFile.Flush();
+            }
+        }
+
+        //Use **Item.Load()** to load this module.
+        //Use **List<Item> ItemList = Item.Load();** to load this module and return the ItemList.
+        //Currently only loads hardcoded file.
+        public static List<Item> Load()
+        {
+            List<Item> ItemList = new List<Item>();
+            Form1.listView1.Items.Clear();
+            string Catagory = "snacks";
+
+            //if (!File.Exists($"./{Catagory}.txt")) { Console.WriteLine("File not Found"); Main(); }
+            if (!File.Exists($"D:/Test/{Catagory}.txt")) { Console.WriteLine("File not Found"); }//TEMP Program.Main(); }
+            //string[] LoadFile = File.ReadAllLines($"./{Catagory}.txt");
+            string[] LoadFile = File.ReadAllLines($"D:/Test/{Catagory}.txt");
+            foreach (string Line in LoadFile)
+            {
+                //for (int Col = 0; Col < 5; Col++)
+                //{
+                //Console.WriteLine(Line);
+                string[] Field = Line.Split('^');
+                //ItemsArray[ItemsCount, Col] = Field[Col];
+                //Form1.listView1.Items.Add(new ListViewItem(new string[] { Field[0].ToString(), Field[3].ToString() }));
+
+                Form1.listView1.Items.Add(new ListViewItem(new string[] { Field[0], Field[1], Field[2], Convert.ToDouble(Field[3]).ToString("C"), Field[4], (Convert.ToDouble(Field[3]) / Convert.ToDouble(Field[2])).ToString("C"), (Convert.ToDouble(Field[3]) / Convert.ToDouble(Field[4])).ToString("C") }));
+                ItemList.Add(new Item() {
+                    ItemName = Field[0],
+                    ItemCat = Field[1],
+                    ItemSize = Convert.ToDouble(Field[2]),
+                    ItemPrice = Convert.ToDouble(Field[3]),
+                    ItemMeals = Convert.ToDouble(Field[4]) });
+                //}
+
+            }
+            Console.WriteLine(ItemList[0].ItemName);
+            Item.Print(ItemList);
+            return ItemList;
+
+        }
+
+
+        // Default comparer for Item type.
+        public int CompareTo(Item compareItem)
+        {
+            // A null value means that this object is greater.
+            if (compareItem == null) { return 1; }
+
+            //else { return this.ItemPrice.CompareTo(compareItem.ItemPrice); }
+            else { return this.ItemPrice.CompareTo(compareItem.ItemPrice); }
+        }
+
+    }
+}
