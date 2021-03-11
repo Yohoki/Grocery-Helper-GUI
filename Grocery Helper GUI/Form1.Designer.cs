7d65721a31c692a206898602711747fa61e88668
Yohoki <Yohoki3@aol.com>
2021-03-11 00:19:29 -0600

    Added Saving and Loading. Saving currently saves to (filename)2 for testing reasons.
    added a weird way of starting the app, because Item.ItemList needed declared, but I didn't want it to be reliant on having a file to load. So, it is declared, loads a blanks list, and then clears itself. There's probably a better way to do this but I've not discovered it yet.

--- Grocery Helper GUI/Form1.Designer.cs
+++ Grocery Helper GUI/Form1.Designer.cs
@@ -29,7 +29,14 @@ namespace Grocery_Helper_GUI
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            //System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem ItemName = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem ItemCat = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem ItemSize = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem ItemPrice = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem ItemMeals = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem ItemPPS = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem ItemPPM = new System.Windows.Forms.ListViewItem("");
             this.button1 = new System.Windows.Forms.Button();
             listView1 = new System.Windows.Forms.ListView();
             this.ColumnItemName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -39,11 +46,17 @@ namespace Grocery_Helper_GUI
             this.ColumnItemMeals = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.ColumnItemPPS = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.ColumnItemPPM = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            textBoxCat = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 265);
+            this.button1.Location = new System.Drawing.Point(213, 415);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(131, 23);
             this.button1.TabIndex = 0;
@@ -63,7 +76,14 @@ namespace Grocery_Helper_GUI
             this.ColumnItemPPM});
             listView1.HideSelection = false;
             listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1});
+            ItemName,
+            ItemCat,
+            ItemSize,
+            ItemPrice,
+            ItemMeals,
+            ItemPPS,
+            ItemPPM});
+            //listViewItem2});
             listView1.Location = new System.Drawing.Point(342, 12);
             listView1.Name = "listView1";
             listView1.Size = new System.Drawing.Size(657, 426);
@@ -101,15 +121,68 @@ namespace Grocery_Helper_GUI
             // 
             this.ColumnItemPPM.Text = "ItemPPM";
             // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(162, 21);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 2;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(243, 21);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(18, 80);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 4;
+            // 
+            // textBoxCat
+            // 
+            textBoxCat.Location = new System.Drawing.Point(6, 21);
+            textBoxCat.Name = "textBoxCat";
+            textBoxCat.Size = new System.Drawing.Size(150, 22);
+            textBoxCat.TabIndex = 5;
+            textBoxCat.Text = "Snacks";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(textBoxCat);
+            this.groupBox1.Controls.Add(this.buttonLoad);
+            this.groupBox1.Controls.Add(this.buttonSave);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(324, 62);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Saving and Loading";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1016, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(listView1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -124,6 +197,11 @@ namespace Grocery_Helper_GUI
         private System.Windows.Forms.ColumnHeader ColumnItemMeals;
         private System.Windows.Forms.ColumnHeader ColumnItemPPS;
         private System.Windows.Forms.ColumnHeader ColumnItemPPM;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.ListBox listBox1;
+        public static System.Windows.Forms.TextBox textBoxCat;
+        private System.Windows.Forms.GroupBox groupBox1;
         public static System.Windows.Forms.ListView listView1;
     }
 }
--- Grocery Helper GUI/Form1.cs
+++ Grocery Helper GUI/Form1.cs
@@ -8,11 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Diagnostics;
 
 namespace Grocery_Helper_GUI
 {
     public partial class Form1 : Form
     {
+        List<Item> ItemList = new List<Item>(Item.Load());
+
         public Form1()
         {
             InitializeComponent();
@@ -20,15 +23,28 @@ namespace Grocery_Helper_GUI
 
         private void button1_Click(object sender, EventArgs e)
         {
-            List<Item> ItemList = Item.Load();
-            //listView1.Items.Add(new ListViewItem(ItemList.ToString()));
+            ItemList = Item.Load(ItemList);
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            ItemList.Clear();
+            Item.Load(ItemList);
+            Item.Print(ItemList);
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(Form1.listView1.ItemName.text.ToString());
+            Item.Save(ItemList);
+            //MessageBox.Show(Item.ItemName);
         }
     }
 
     public class Item : IEquatable<Item>, IComparable<Item>//, IEnumerable<Item>
     {
-        //Use **ItemList.Add(new Item() { ItemName = "Test", ItemCat="snackos", ItemSize = 6, ItemPrice = 5.00, ItemMeals = 2 });** to set values
-        //Set ItemName in ItemsList
+            //Use **ItemList.Add(new Item() { ItemName = "Test", ItemCat="snackos", ItemSize = 6, ItemPrice = 5.00, ItemMeals = 2 });** to set values
+            //Set ItemName in ItemsList
         public string ItemName { get; set; }
 
         //Set ItemCat in ItemsList
@@ -42,6 +58,7 @@ namespace Grocery_Helper_GUI
 
         //Set ItemPrice in ItemsList
         public double ItemPrice { get; set; }
+
 
 
         //Use WriteLine(ItemList) to display this string.
@@ -91,7 +108,7 @@ namespace Grocery_Helper_GUI
 
             foreach (Item aItem in ItemList)
             {
-                Form1.listView1.Items.Add(new ListViewItem(new string[] {
+                Form1.listView1.Items.Add(new ListViewItem (new string[] {
                     ItemList[ID].ItemName,
                     ItemList[ID].ItemCat,
                     ItemList[ID].ItemSize.ToString(),
@@ -102,17 +119,18 @@ namespace Grocery_Helper_GUI
                 }));
                 ID++;
             }
+            //temp MessageBox.Show(Form1.listView1.Items[1].ToString());
             return ItemList;
         }
 
         //Use **Item.Save(ItemList);** to Save ItemsList to a file with '^' delimiters.
         //CURRENTLY: uses ItemsList[0].ItemCat to determine filename.
-        public static void Save(List<Item> ItemsList)
+        public static List<Item> Save(List<Item> ItemList)
         {
             //string SaveLine ="";
             //StreamWriter SaveFile = File.AppendText($"./{ItemsList[0].ItemCat}.txt");
-            StreamWriter SaveFile = File.AppendText($"D:/Test/{ItemsList[0].ItemCat}.txt");
-            foreach (Item aItem in ItemsList)
+            StreamWriter SaveFile = File.AppendText($"D:/Test/{Form1.textBoxCat.Text.ToString()}2.txt");
+            foreach (Item aItem in ItemList)
             {
                 //SaveLine = SaveLine + aItem + "^";
                 SaveFile.WriteLine(
@@ -124,34 +142,69 @@ namespace Grocery_Helper_GUI
                 );
                 SaveFile.Flush();
             }
+            SaveFile.Close();
+            Process.Start("notepad.exe", $"D:/Test/{Form1.textBoxCat.Text.ToString()}2.txt");
+            return ItemList;
         }
 
         //Use **Item.Load()** to load this module.
         //Use **List<Item> ItemList = Item.Load();** to load this module and return the ItemList.
         //Currently only loads hardcoded file.
-        public static List<Item> Load()
+        public static List<Item> Load(List<Item> ItemList)
         {
-            List<Item> ItemList = new List<Item>();
+            //List<Item> ItemList = new List<Item>();
             string Catagory = "snacks";
             //int ID = 0;
 
             //if (!File.Exists($"./{Catagory}.txt")) { Console.WriteLine("File not Found"); Main(); }
-            if (!File.Exists($"D:/Test/{Catagory}.txt")) { Console.WriteLine("File not Found"); }
+            if (!File.Exists($"D:/Test/{Form1.textBoxCat.Text?.ToLower()}.txt")) { Console.WriteLine("File not Found"); return ItemList; }
             //string[] LoadFile = File.ReadAllLines($"./{Catagory}.txt");
-            string[] LoadFile = File.ReadAllLines($"D:/Test/{Catagory}.txt");
-            foreach (string Line in LoadFile)
+            if (!String.IsNullOrEmpty(Form1.textBoxCat.Text))
             {
-                string[] Field = Line.Split('^');
-                
-                ItemList.Add(new Item() {
-                    ItemName = Field[0],
-                    ItemCat = Field[1],
-                    ItemSize = Convert.ToDouble(Field[2]),
-                    ItemPrice = Convert.ToDouble(Field[3]),
-                    ItemMeals = Convert.ToDouble(Field[4])
-                });
+                string[] LoadFile = File.ReadAllLines($"D:/Test/{Form1.textBoxCat.Text?.ToLower()}.txt");
+                //string[] LoadFile = File.ReadAllLines($"D:/Test/{Catagory.ToLower()}.txt");
+
+                foreach (string Line in LoadFile)
+                {
+                    string[] Field = Line.Split('^');
+
+                    ItemList.Add(new Item()
+                    {
+                        ItemName = Field[0],
+                        ItemCat = Field[1],
+                        ItemSize = Convert.ToDouble(Field[2]),
+                        ItemPrice = Convert.ToDouble(Field[3]),
+                        ItemMeals = Convert.ToDouble(Field[4])
+                    });
+                }
             }
-            Item.Print(ItemList);
+            else { return ItemList; }
+            return ItemList;
+        }
+        public static List<Item> Load()
+        {
+            List<Item> ItemList = new List<Item>();
+            //int ID = 0;
+
+            //if (!File.Exists($"./{Catagory}.txt")) { Console.WriteLine("File not Found"); Main(); }
+            //if (!File.Exists($"D:/Test/{Catagory}.txt")) { Console.WriteLine("File not Found"); }
+            //string[] LoadFile = File.ReadAllLines($"./{Catagory}.txt");
+            //string[] LoadFile = File.ReadAllLines($"D:/Test/{Catagory}.txt");
+                //string[] LoadFile = File.ReadAllLines($"D:/Test/{Catagory.ToLower()}.txt");
+
+            //foreach (string Line in LoadFile)
+            //{
+            //    string[] Field = Line.Split('^');
+                
+                ItemList.Add(new Item()
+                {
+                    ItemName = "",
+                    ItemCat = "",
+                    ItemSize = 0,
+                    ItemPrice = 0,
+                    ItemMeals = 0
+                });
+            //}
             return ItemList;
         }
 
@@ -165,6 +218,6 @@ namespace Grocery_Helper_GUI
             //else { return this.ItemPrice.CompareTo(compareItem.ItemPrice); }
             else { return this.ItemPrice.CompareTo(compareItem.ItemPrice); }
         }
-
     }
+
 }
